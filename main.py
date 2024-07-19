4850c747a94b5ee1a7d76d3a6d49a416651e5d55
Rudransh Pratap Singh <87255169+CoderFleet@users.noreply.github.com>
2024-07-19 09:42:59 +0530

    Added Control Btn & duration

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def tit_for_tat(player, opponent):
 def random_strategy(player, opponent):
     return random.choice([COOPERATE, DEFECT])
 
-def play_round(player1, player2):
+def play_round(player1, player2, log_widget):
     action1 = player1.choose_action(player2)
     action2 = player2.choose_action(player1)
     
@@ -42,21 +42,39 @@ def play_round(player1, player2):
     elif action1 == DEFECT and action2 == DEFECT:
         player1.score += 1
         player2.score += 1
+    
+    log_widget.insert(tk.END, f"Round: {len(player1.history)}, {player1.name} action: {action1}, {player2.name} action: {action2}\n")
+    log_widget.see(tk.END)
 
-def update_gui(round_num, player1, player2, label1, label2, progress1, progress2):
+def update_gui(player1, player2, label1, label2, progress1, progress2):
     label1.configure(text=f"{player1.name} strategy: {player1.history[-1]} | Score: {player1.score}")
     label2.configure(text=f"{player2.name} strategy: {player2.history[-1]} | Score: {player2.score}")
     progress1['value'] = player1.score
     progress2['value'] = player2.score
 
-def simulate_rounds(player1, player2, num_rounds, label1, label2, progress1, progress2):
-    for round_num in range(1, num_rounds + 1):
-        play_round(player1, player2)
-        update_gui(round_num, player1, player2, label1, label2, progress1, progress2)
+def simulate_rounds(player1, player2, num_rounds, label1, label2, progress1, progress2, log_widget):
+    for _ in range(num_rounds):
+        play_round(player1, player2, log_widget)
+        update_gui(player1, player2, label1, label2, progress1, progress2)
         label1.update()
         label2.update()
         progress1.update()
         progress2.update()
+
+def start_simulation(player1, player2, num_rounds_var, label1, label2, progress1, progress2, log_widget):
+    num_rounds = int(num_rounds_var.get())
+    simulate_rounds(player1, player2, num_rounds, label1, label2, progress1, progress2, log_widget)
+
+def reset_simulation(player1, player2, label1, label2, progress1, progress2, log_widget):
+    player1.score = 0
+    player2.score = 0
+    player1.history = []
+    player2.history = []
+    label1.configure(text="")
+    label2.configure(text="")
+    progress1['value'] = 0
+    progress2['value'] = 0
+    log_widget.delete(1.0, tk.END)
 
 def main():
     player1 = Player("Player 1", tit_for_tat)
@@ -75,7 +93,20 @@ def main():
     progress2 = ttk.Progressbar(root, orient=tk.HORIZONTAL, length=200, mode='determinate')
     progress2.pack(pady=10)
     
-    simulate_rounds(player1, player2, 10, label1, label2, progress1, progress2)
+    num_rounds_var = tk.StringVar(value="10")
+    num_rounds_label = ttk.Label(root, text="Number of Rounds:")
+    num_rounds_label.pack(pady=5)
+    num_rounds_entry = ttk.Entry(root, textvariable=num_rounds_var)
+    num_rounds_entry.pack(pady=5)
+    
+    start_button = ttk.Button(root, text="Start Simulation", command=lambda: start_simulation(player1, player2, num_rounds_var, label1, label2, progress1, progress2, log_widget))
+    start_button.pack(pady=10)
+    
+    reset_button = ttk.Button(root, text="Reset Simulation", command=lambda: reset_simulation(player1, player2, label1, label2, progress1, progress2, log_widget))
+    reset_button.pack(pady=10)
+    
+    log_widget = tk.Text(root, height=10, width=50)
+    log_widget.pack(pady=10)
     
     root.mainloop()
 
